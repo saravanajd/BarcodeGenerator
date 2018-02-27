84085d7a8d2947433497c6107704df4f64758441
saravanajd <saravana.jd@outlook.com>
2018-02-27 15:47:32 +0530

    Chagne the versions

--- WinForm Test/Properties/AssemblyInfo.cs
+++ WinForm Test/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WinForm Test")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Barcode Generator")]
+[assembly: AssemblyDescription("Create By Saravanan")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WinForm Test")]
+[assembly: AssemblyProduct("Barcode Generator")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
